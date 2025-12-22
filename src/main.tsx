3ef677b6db8a7d83c4482ec16568ac1d8a6d5e64
Wenjie <wenjie.egerbladh@gmail.com>
2025-12-22 15:38:47 +0100

    Add TanStack React Router and initial routes setup
    Integrate @tanstack/react-router and related devtools, update Vite config to use the router plugin, and add initial route components and generated route tree. Also add .prettierignore for generated and build files.

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen.ts'
+
+const router = createRouter({
+  routeTree,
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider storageKey="vite-ui-theme">
+      <RouterProvider router={router}></RouterProvider>
+    </ThemeProvider>
   </StrictMode>
 )
