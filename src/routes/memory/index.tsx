252bd66ab72ed9936b49156fb30e65826a63373f
Alice Karlén <alice.karlen@hotmail.com>
2026-01-23 16:06:11 +0100

    Add pointing system. Connect with point counter-comp. Connect with result

--- src/routes/memory/index.tsx
+++ src/routes/memory/index.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { GridComponent } from '@/components/grid-component'
-import { Button } from '@/components/ui/button'
-import { ArrowLeft, Pointer, RotateCcw } from 'lucide-react'
+import { useState, useCallback } from "react";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { GridComponent } from "@/components/grid-component";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, Pointer, RotateCcw } from "lucide-react";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
-} from '@/components/ui/tooltip'
-import { Dialog as DialogPrimitive } from "radix-ui"
-import { TimerComponent } from '@/components/timer-component'
-import { useStopwatch } from 'react-timer-hook'
-import { SettingsDialog } from '@/components/settings-component'
-import { ConfirmationPopup } from '@/components/confirmation-popup'
-import { DifficultyBar } from '@/components/difficulty-bar'
-import { PointBar } from '@/components/point-counter-component'
+} from "@/components/ui/tooltip";
+import { Dialog as DialogPrimitive } from "radix-ui";
+import { TimerComponent } from "@/components/timer-component";
+import { useStopwatch } from "react-timer-hook";
+import { SettingsDialog } from "@/components/settings-component";
+import { ConfirmationPopup } from "@/components/confirmation-popup";
+import { DifficultyBar } from "@/components/difficulty-bar";
+import { PointBar } from "@/components/point-counter-component";
 
 export const Route = createFileRoute("/memory/")({
   component: RouteComponent,
@@ -23,8 +23,16 @@ export const Route = createFileRoute("/memory/")({
 function RouteComponent() {
   const router = useRouter();
 
+  // point counter
+  const [points, setPoints] = useState(0);
+  const handlePointsChange = (newP: number) => {
+    setPoints((prevP) => prevP + newP);
+  };
+
   // stopwatch stuff
-  const { seconds, minutes, pause, start, isRunning } = useStopwatch({ autoStart: false });
+  const { seconds, minutes, pause, start, isRunning } = useStopwatch({
+    autoStart: false,
+  });
 
   // state för resultat
   const [result, setResult] = useState<{
@@ -34,10 +42,16 @@ function RouteComponent() {
   } | null>(null);
 
   // funktion för att avsluta spelet och spara resultat
-  const finishGame = () => {
+  const finishGame = useCallback(() => {
+    if (result) return;
+
+    pause();
+
+    const totalSeconds = minutes * 60 + seconds;
+
     const newResult = {
-      score: 120,
-      time: 85,
+      score: points,
+      time: totalSeconds,
       date: new Date().toISOString(),
     };
 
@@ -46,7 +60,7 @@ function RouteComponent() {
     const prev = JSON.parse(localStorage.getItem("memoryResults") ?? "[]");
 
     localStorage.setItem("memoryResults", JSON.stringify([...prev, newResult]));
-  };
+  }, [minutes, seconds, points, pause, result]);
 
   // topplista (topp 5)
   const topResults: {
@@ -75,8 +89,12 @@ function RouteComponent() {
               <Pointer />
             </Button>
           </DialogPrimitive.Close>
-          <div className='flex flex-row items-start'>
-            <Button variant={"default"} className=" m-1 h-8 p-0.5 text-xs cursor-pointer" onClick={() => router.history.back()}>
+          <div className="flex flex-row items-start">
+            <Button
+              variant={"default"}
+              className=" m-1 h-8 p-0.5 text-xs cursor-pointer"
+              onClick={() => router.history.back()}
+            >
               <ArrowLeft />
               Tillbaka
             </Button>
@@ -89,7 +107,11 @@ function RouteComponent() {
           <Tooltip>
             <TooltipTrigger asChild>
               <DialogPrimitive.Trigger>
-                <Button className="cursor-pointer" variant={'ghost'} onClick={pause}>
+                <Button
+                  className="cursor-pointer"
+                  variant={"ghost"}
+                  onClick={pause}
+                >
                   <ArrowLeft />
                 </Button>
               </DialogPrimitive.Trigger>
@@ -100,7 +122,7 @@ function RouteComponent() {
           </Tooltip>
           <DialogPrimitive.Overlay className="fixed inset-0 bg-black/40 z-40" />
           <DialogPrimitive.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg z-60 flex flex-col text-center items-center border-2">
-            <ConfirmationPopup linkTo='/' leaveGameMem onStart={start} />
+            <ConfirmationPopup linkTo="/" leaveGameMem onStart={start} />
           </DialogPrimitive.Content>
         </DialogPrimitive.Root>
 
@@ -125,13 +147,22 @@ function RouteComponent() {
       </div>
 
       <div className="flex flex-col *:items-start">
-        <div className='flex flex-row justify-between'>
+        <div className="flex flex-row justify-between">
           <SettingsDialog onStart={start} onPause={pause} />
           <DifficultyBar />
-          <PointBar />
-          <TimerComponent minutes={minutes} seconds={seconds} onStart={start} onPause={pause} isRunning={isRunning} />
+          <PointBar points={points} />
+          <TimerComponent
+            minutes={minutes}
+            seconds={seconds}
+            onStart={start}
+            onPause={pause}
+            isRunning={isRunning}
+          />
         </div>
-        <GridComponent />
+        <GridComponent
+          onPointsChange={handlePointsChange}
+          onGameComplete={finishGame}
+        />
         <Button onClick={finishGame} className="w-fit self-">
           Visa resultat
         </Button>
@@ -143,16 +174,17 @@ function RouteComponent() {
               <span className="font-bold">Poäng:</span> {result.score}
             </p>
             <p>
-              <span className="font-bold">Tid:</span> {formatTime(result.time)}{" "}
+              <span className="font-bold">Tid:</span>{" "}
+              {formatTime(result.time)}{" "}
             </p>
             <p>
-              <span className="font-bold"> Datum:</span>{' '}
-              {new Date(result.date).toLocaleString('sv-SE', {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit',
+              <span className="font-bold"> Datum:</span>{" "}
+              {new Date(result.date).toLocaleString("sv-SE", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
               })}
             </p>
           </div>
@@ -173,6 +205,6 @@ function RouteComponent() {
           </ol>
         )}
       </div>
-    </main >
+    </main>
   );
 }
