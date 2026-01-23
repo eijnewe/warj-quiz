6ab1e3864d53137ae8e1ddcef4f2d1a541a15d81
Alice Karlén <alice.karlen@hotmail.com>
2026-01-23 12:52:20 +0100

    Card flip animation and functionality for flipping rules. Style for matched/non-matched cards.

--- src/routes/quiz/test.tsx
+++ src/routes/quiz/test.tsx
@@ -1,14 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { MemoryCard } from '@/components/memory-card-component'
-import { DifficultyBar } from '@/components/difficulty-component'
-import { GridComponent1 } from '@/components/grid-component'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/quiz/test')({
+export const Route = createFileRoute("/quiz/test")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <>
-    <div>Hello "/quiz/test"!</div>
-  </>
+  return (
+    <>
+      <div>Hello "/quiz/test"!</div>
+    </>
+  );
 }
